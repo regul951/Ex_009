f3e309e22cd2ecdc3e20c3dc26be2cbc60caf1b9
regul951 <regul951@gmail.com>
2023-11-19 16:28:09 +0300

    Решение задачи 66

--- Program.cs
+++ Program.cs
@@ -5,28 +5,53 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
-static void PrintNums(int M, int N)
+static void PrintEvenNums(int M, int N)
 {
     int divider = 2;
 
     if (M % divider == 0)
     {
-        if (M + divider <= N)
+        if (M + divider <= N) //рекуррентный случай
         {
             Console.Write($"{M}, ");
-            PrintNums(M + divider, N);
+            PrintEvenNums(M + divider, N);
         }
-        else
+        else //крайний случай
         {
             Console.Write($"{M}\n");
         }
     }
     else
     {
-        PrintNums(M + 1, N);
+        PrintEvenNums(M + 1, N);
     }
 }
 
-
 Console.WriteLine("Задача 64");
-PrintNums(4, 8);
+PrintEvenNums(4, 8);
+
+
+
+// Задача 66
+// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N 
+// с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+static int SumNums(int M, int N)
+{
+    int sum = M;
+
+    if (M < N)
+    {
+        sum += SumNums(M + 1, N);
+    }
+
+    return sum;
+}
+
+Console.WriteLine("Задача 66");
+Console.WriteLine(SumNums(4, 8));
+
+
